30b45a1c8a540c5c6e37b02f3613f4b0fbc6759d
Vladyslav Zyma <vladzima13@gmail.com>
2023-07-10 12:15:06 +0300

    added focus on search input and remove all movies from list by clicking "enter" button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,21 @@
+import { useRef, useEffect } from "react";
+import { useKey } from "../hooks/useKey";
+
 function Search({ query, onSetQuery }) {
+  const inputEl = useRef(null);
+
+  //focus on search input by clicking 'Enter' button
+  useKey("Enter", function () {
+    if (document.activeElement === inputEl.current) return;
+
+    inputEl.current.focus();
+    onSetQuery("");
+  });
+
   return (
     <input
       className="search"
+      ref={inputEl}
       type="text"
       placeholder="Search movies..."
       value={query}
